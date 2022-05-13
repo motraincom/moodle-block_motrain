68faf06f591590419a68a8e26d9ed96103fac733
Frédéric Massart <fred@branchup.tech>
2022-05-13 18:36:57 +0800

    Bump to v1.0.0

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2022051301;
+$plugin->version  = 2022051302;
 $plugin->requires = 2017051500;     // Moodle 3.3.0.
 $plugin->release = '1.0.0';
 $plugin->maturity = MATURITY_STABLE;
