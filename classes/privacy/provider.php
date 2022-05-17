b660391e50d7340d4ce64347e2cfd482167611c9
Frédéric Massart <fred@branchup.tech>
2022-05-17 18:01:47 +0800

    Implement Privacy API

---
+++ classes/privacy/provider.php
@@ -0,0 +1,242 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Data provider.
+ *
+ * @package    block_motrain
+ * @copyright  2022 Mootivation Technologies Corp.
+ * @author     Frédéric Massart <fred@branchup.tech>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace block_motrain\privacy;
+defined('MOODLE_INTERNAL') || die();
+
+use context;
+use context_system;
+use core_privacy\local\metadata\collection;
+use core_privacy\local\request\approved_contextlist;
+use core_privacy\local\request\contextlist;
+use core_privacy\local\request\contextlist_base;
+use core_privacy\local\request\transform;
+use core_privacy\local\request\writer;
+
+/**
+ * Data provider class.
+ *
+ * @package    block_motrain
+ * @copyright  2022 Mootivation Technologies Corp.
+ * @author     Frédéric Massart <fred@branchup.tech>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class provider implements
+    \core_privacy\local\metadata\provider,
+    \core_privacy\local\request\plugin\provider {
+
+    use \core_privacy\local\legacy_polyfill;
+
+    /**
+     * Returns metadata.
+     *
+     * @param collection $collection The initialised collection to add items to.
+     * @return collection A listing of user data stored through this system.
+     */
+    public static function _get_metadata(collection $collection) {
+        $collection->add_database_table('block_motrain_playermap', [
+            'userid' => 'privacy:metadata:playermap:userid',
+            'accountid' => 'privacy:metadata:playermap:accountid',
+            'playerid' => 'privacy:metadata:playermap:playerid',
+            'blocked' => 'privacy:metadata:playermap:blocked',
+            'blockedreason' => 'privacy:metadata:playermap:blockedreason',
+        ], 'privacy:metadata:playermap');
+
+        $collection->add_database_table('block_motrain_log', [
+            'userid' => 'privacy:metadata:log:userid',
+            'contextid' => 'privacy:metadata:log:contextid',
+            'coins' => 'privacy:metadata:log:coins',
+            'actionname' => 'privacy:metadata:log:actionname',
+            'actionhash' => 'privacy:metadata:log:actionhash',
+            'timecreated' => 'privacy:metadata:log:timecreated',
+            'timebroadcasted' => 'privacy:metadata:log:timebroadcasted',
+            'broadcasterror' => 'privacy:metadata:log:broadcasterror'
+        ], 'privacy:metadata:log');
+
+        // No need to export data for this, it is just a queue and contains nothing.
+        $collection->add_database_table('block_motrain_userspush', [
+            'userid' => 'privacy:metadata:userspush:userid',
+        ], 'privacy:metadata:userspush');
+
+        // When submitting coins.
+        $collection->add_external_location_link('coinsgained', [
+            'coins' => 'privacy:metadata:coinsgained:coins',
+            'reason' => 'privacy:metadata:coinsgained:reason',
+        ], 'privacy:metadata:coinsgained');
+
+        // When creating/searching user.
+        $collection->add_external_location_link('remoteplayer', [
+            'firstname' => 'privacy:metadata:remoteplayer:firstname',
+            'lastname' => 'privacy:metadata:remoteplayer:lastname',
+            'email' => 'privacy:metadata:remoteplayer:email',
+        ], 'privacy:metadata:remoteplayer');
+
+        return $collection;
+    }
+    /**
+     * Get the list of contexts that contain user information for the specified user.
+     *
+     * @param int $userid The user to search.
+     * @return contextlist $contextlist The contextlist containing the list of contexts used in this plugin.
+     */
+    public static function _get_contexts_for_userid($userid) {
+        global $DB;
+        $contextlist = new \core_privacy\local\request\contextlist();
+
+        if ($DB->record_exists('block_motrain_playermap', ['userid' => $userid])) {
+            $contextlist->add_system_context();
+        }
+
+        $sql = 'SELECT l.contextid FROM {block_motrain_log} l WHERE l.userid = ?';
+        $params = [$userid];
+        $contextlist->add_from_sql($sql, $params);
+
+        return $contextlist;
+    }
+
+    /**
+     * Export all user data for the specified user, in the specified contexts.
+     *
+     * @param approved_contextlist $contextlist The approved contexts to export information for.
+     */
+    public static function _export_user_data(approved_contextlist $contextlist) {
+        global $DB;
+
+        if (!$contextlist->count()) {
+            return;
+        }
+
+        $user = $contextlist->get_user();
+        $userid = $user->id;
+
+        // Export logs.
+        $path = [get_string('pluginname', 'block_motrain'), get_string('privacy:path:logs', 'block_motrain')];
+        $flushlogs = function($contextid, $logs) use ($path) {
+            $context = context::instance_by_id($contextid);
+            writer::with_context($context)->export_data($path, (object) ['data' => $logs]);
+        };
+
+        list($insql, $inparams) = $DB->get_in_or_equal($contextlist->get_contextids(), SQL_PARAMS_NAMED);
+        $sql = "contextid $insql AND userid = :userid";
+        $params = ['userid' => $userid] + $inparams;
+        $recordset = $DB->get_recordset_select('block_motrain_log', $sql, $params, 'contextid ASC, id ASC');
+        $lastcontextid = null;
+        $logs = [];
+        foreach ($recordset as $record) {
+            if ($lastcontextid && $lastcontextid != $record->contextid) {
+                $flushlogs($lastcontextid, $logs);
+                $logs = [];
+            }
+
+            $logs[] = (object) [
+                'userid' => transform::user($record->userid),
+                'coins' => $record->coins,
+                'action_name' => $record->actionname,
+                'action_hash' => $record->actionhash,
+                'timecreated' => $record->timecreated ? transform::date($record->timecreated) : '-',
+                'timebroadcasted' => $record->timebroadcasted ? transform::date($record->timebroadcasted) : '-'
+            ];
+            $lastcontextid = $record->contextid;
+        }
+
+        // Flush the last iteration.
+        if ($lastcontextid) {
+            $flushlogs($lastcontextid, $logs);
+        }
+        $recordset->close();
+
+        // Export mapping.
+        $data = [];
+        if (static::contains_system($contextlist)) {
+            $recordset = $DB->get_recordset('block_motrain_playermap', ['userid' => $userid], 'id ASC');
+            $data = [];
+            foreach ($recordset as $record) {
+                $data[] = (object) [
+                    'userid' => transform::user($record->userid),
+                    'accountid' => $record->accountid,
+                    'playerid' => $record->playerid,
+                    'blocked' => $record->blocked,
+                    'blockedreason' => $record->blockedreason,
+                ];
+            }
+            $recordset->close();
+        }
+        if (!empty($data)) {
+            writer::with_context(context_system::instance())->export_data([
+                get_string('pluginname', 'block_motrain'),
+                get_string('privacy:path:mappings', 'block_motrain')
+            ], (object) ['data' => $data]);
+        }
+    }
+
+    /**
+     * Delete all data for all users in the specified context.
+     *
+     * @param context $context The specific context to delete data for.
+     */
+    public static function _delete_data_for_all_users_in_context(context $context) {
+        global $DB;
+
+        if ($context->contextlevel == CONTEXT_SYSTEM) {
+            $DB->delete_records('block_motrain_playermap', []);
+        }
+
+        $DB->delete_records('block_motrain_log', ['contextid' => $context->id]);
+    }
+
+    /**
+     * Delete all user data for the specified user, in the specified contexts.
+     *
+     * @param approved_contextlist $contextlist The approved contexts and user information to delete information for.
+     */
+    public static function _delete_data_for_user(approved_contextlist $contextlist) {
+        global $DB;
+
+        $user = $contextlist->get_user();
+        $userid = $user->id;
+
+        foreach ($contextlist as $context) {
+            if ($context->contextlevel == CONTEXT_SYSTEM) {
+                $DB->delete_records('block_motrain_playermap', ['userid' => $userid]);
+            }
+            $DB->delete_records('block_motrain_log', ['contextid' => $context->id, 'userid' => $userid]);
+        }
+    }
+
+    /**
+     * Whether a context list contains system.
+     *
+     * @param contextlist_base $contextlist The list.
+     */
+    protected static function contains_system(contextlist_base $contextlist) {
+        foreach ($contextlist as $context) {
+            if ($context->contextlevel == CONTEXT_SYSTEM) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
---
+++ tests/privacy_provider_test.php
@@ -0,0 +1,308 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Test privacy provider.
+ *
+ * @package    block_motrain
+ * @copyright  2022 Mootivation Technologies Corp.
+ * @author     Frédéric Massart <fred@branchup.tech>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+
+use core_privacy\local\metadata\collection;
+use core_privacy\local\request\writer;
+use core_privacy\tests\request\approved_contextlist;
+use block_motrain\privacy\provider;
+
+/**
+ * Privacy provider testcase.
+ *
+ * @package    block_motrain
+ * @copyright  2022 Mootivation Technologies Corp.
+ * @author     Frédéric Massart <fred@branchup.tech>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class block_motrain_privacy_provider_testcase extends \advanced_testcase {
+
+    public function test_get_metadata() {
+        $data = provider::get_metadata(new collection('block_motrain'));
+        $this->assertCount(5, $data->get_collection());
+    }
+
+    public function test_get_contexts_for_userid() {
+        $this->resetAfterTest(true);
+        extract($this->generate_test_data());
+
+        $contextlist = provider::get_contexts_for_userid($u1->id);
+        $this->assert_contextlist_equals($contextlist, [SYSCONTEXTID, context_course::instance($c1->id)->id,
+            context_course::instance($c2->id)->id]);
+        $contextlist = provider::get_contexts_for_userid($u2->id);
+        $this->assert_contextlist_equals($contextlist, [SYSCONTEXTID, context_course::instance($c1->id)->id,]);
+        $contextlist = provider::get_contexts_for_userid($u3->id);
+        $this->assert_contextlist_equals($contextlist, [SYSCONTEXTID]);
+        $contextlist = provider::get_contexts_for_userid($u4->id);
+        $this->assert_contextlist_equals($contextlist, []);
+    }
+
+    public function test_delete_data_for_all_users_in_context() {
+        global $DB;
+
+        $this->resetAfterTest(true);
+        extract($this->generate_test_data());
+
+        $c1ctx = context_course::instance($c1->id);
+        $c2ctx = context_course::instance($c2->id);
+
+        $this->assertTrue($DB->record_exists('block_motrain_log', []));
+        $this->assertTrue($DB->record_exists('block_motrain_log', ['contextid' => SYSCONTEXTID]));
+        $this->assertTrue($DB->record_exists('block_motrain_log', ['contextid' => $c1ctx->id]));
+        $this->assertTrue($DB->record_exists('block_motrain_log', ['contextid' => $c2ctx->id]));
+        $this->assertTrue($DB->record_exists('block_motrain_playermap', []));
+
+        provider::delete_data_for_all_users_in_context(context_course::instance($c1->id));
+
+        $this->assertTrue($DB->record_exists('block_motrain_log', []));
+        $this->assertTrue($DB->record_exists('block_motrain_log', ['contextid' => SYSCONTEXTID]));
+        $this->assertFalse($DB->record_exists('block_motrain_log', ['contextid' => $c1ctx->id]));
+        $this->assertTrue($DB->record_exists('block_motrain_log', ['contextid' => $c2ctx->id]));
+        $this->assertTrue($DB->record_exists('block_motrain_playermap', []));
+
+        provider::delete_data_for_all_users_in_context(context_system::instance());
+
+        $this->assertTrue($DB->record_exists('block_motrain_log', []));
+        $this->assertFalse($DB->record_exists('block_motrain_log', ['contextid' => SYSCONTEXTID]));
+        $this->assertFalse($DB->record_exists('block_motrain_log', ['contextid' => $c1ctx->id]));
+        $this->assertTrue($DB->record_exists('block_motrain_log', ['contextid' => $c2ctx->id]));
+        $this->assertFalse($DB->record_exists('block_motrain_playermap', []));
+    }
+
+    public function test_delete_data_for_user() {
+        global $DB;
+
+        $this->resetAfterTest(true);
+        extract($this->generate_test_data());
+
+        $c1ctx = context_course::instance($c1->id);
+        $c2ctx = context_course::instance($c2->id);
+
+        $this->assertTrue($DB->record_exists('block_motrain_log', ['userid' => $u1->id, 'contextid' => SYSCONTEXTID]));
+        $this->assertTrue($DB->record_exists('block_motrain_log', ['userid' => $u1->id, 'contextid' => $c1ctx->id]));
+        $this->assertTrue($DB->record_exists('block_motrain_log', ['userid' => $u1->id, 'contextid' => $c2ctx->id]));
+        $this->assertTrue($DB->record_exists('block_motrain_log', ['userid' => $u2->id, 'contextid' => SYSCONTEXTID]));
+        $this->assertTrue($DB->record_exists('block_motrain_log', ['userid' => $u2->id, 'contextid' => $c1ctx->id]));
+        $this->assertTrue($DB->record_exists('block_motrain_playermap', ['userid' => $u1->id]));
+        $this->assertTrue($DB->record_exists('block_motrain_playermap', ['userid' => $u3->id]));
+
+        $approvedcontexts = new approved_contextlist($u1, 'block_motrain', [$c1ctx->id]);
+        provider::delete_data_for_user($approvedcontexts);
+
+        $this->assertTrue($DB->record_exists('block_motrain_log', ['userid' => $u1->id, 'contextid' => SYSCONTEXTID]));
+        $this->assertFalse($DB->record_exists('block_motrain_log', ['userid' => $u1->id, 'contextid' => $c1ctx->id]));
+        $this->assertTrue($DB->record_exists('block_motrain_log', ['userid' => $u1->id, 'contextid' => $c2ctx->id]));
+        $this->assertTrue($DB->record_exists('block_motrain_log', ['userid' => $u2->id, 'contextid' => SYSCONTEXTID]));
+        $this->assertTrue($DB->record_exists('block_motrain_log', ['userid' => $u2->id, 'contextid' => $c1ctx->id]));
+        $this->assertTrue($DB->record_exists('block_motrain_playermap', ['userid' => $u1->id]));
+        $this->assertTrue($DB->record_exists('block_motrain_playermap', ['userid' => $u3->id]));
+
+        $approvedcontexts = new approved_contextlist($u2, 'block_motrain', [SYSCONTEXTID]);
+        provider::delete_data_for_user($approvedcontexts);
+
+        $this->assertTrue($DB->record_exists('block_motrain_log', ['userid' => $u1->id, 'contextid' => SYSCONTEXTID]));
+        $this->assertFalse($DB->record_exists('block_motrain_log', ['userid' => $u1->id, 'contextid' => $c1ctx->id]));
+        $this->assertTrue($DB->record_exists('block_motrain_log', ['userid' => $u1->id, 'contextid' => $c2ctx->id]));
+        $this->assertFalse($DB->record_exists('block_motrain_log', ['userid' => $u2->id, 'contextid' => SYSCONTEXTID]));
+        $this->assertTrue($DB->record_exists('block_motrain_log', ['userid' => $u2->id, 'contextid' => $c1ctx->id]));
+        $this->assertTrue($DB->record_exists('block_motrain_playermap', ['userid' => $u1->id]));
+        $this->assertTrue($DB->record_exists('block_motrain_playermap', ['userid' => $u3->id]));
+
+        $approvedcontexts = new approved_contextlist($u3, 'block_motrain', [SYSCONTEXTID]);
+        provider::delete_data_for_user($approvedcontexts);
+
+        $this->assertTrue($DB->record_exists('block_motrain_log', ['userid' => $u1->id, 'contextid' => SYSCONTEXTID]));
+        $this->assertFalse($DB->record_exists('block_motrain_log', ['userid' => $u1->id, 'contextid' => $c1ctx->id]));
+        $this->assertTrue($DB->record_exists('block_motrain_log', ['userid' => $u1->id, 'contextid' => $c2ctx->id]));
+        $this->assertFalse($DB->record_exists('block_motrain_log', ['userid' => $u2->id, 'contextid' => SYSCONTEXTID]));
+        $this->assertTrue($DB->record_exists('block_motrain_log', ['userid' => $u2->id, 'contextid' => $c1ctx->id]));
+        $this->assertTrue($DB->record_exists('block_motrain_playermap', ['userid' => $u1->id]));
+        $this->assertFalse($DB->record_exists('block_motrain_playermap', ['userid' => $u3->id]));
+
+        $approvedcontexts = new approved_contextlist($u1, 'block_motrain', [SYSCONTEXTID, $c2ctx->id]);
+        provider::delete_data_for_user($approvedcontexts);
+
+        $this->assertFalse($DB->record_exists('block_motrain_log', ['userid' => $u1->id, 'contextid' => SYSCONTEXTID]));
+        $this->assertFalse($DB->record_exists('block_motrain_log', ['userid' => $u1->id, 'contextid' => $c1ctx->id]));
+        $this->assertFalse($DB->record_exists('block_motrain_log', ['userid' => $u1->id, 'contextid' => $c2ctx->id]));
+        $this->assertFalse($DB->record_exists('block_motrain_log', ['userid' => $u2->id, 'contextid' => SYSCONTEXTID]));
+        $this->assertTrue($DB->record_exists('block_motrain_log', ['userid' => $u2->id, 'contextid' => $c1ctx->id]));
+        $this->assertFalse($DB->record_exists('block_motrain_playermap', ['userid' => $u1->id]));
+        $this->assertFalse($DB->record_exists('block_motrain_playermap', ['userid' => $u3->id]));
+    }
+
+    public function test_extract_user_data() {
+        $this->resetAfterTest(true);
+        extract($this->generate_test_data());
+
+        $c1ctx = context_course::instance($c1->id);
+        $c2ctx = context_course::instance($c2->id);
+
+        $approvedcontexts = new approved_contextlist($u1, 'block_motrain', [SYSCONTEXTID, $c1ctx->id, $c2ctx->id]);
+        provider::export_user_data($approvedcontexts);
+
+        $writer = writer::with_context(context_system::instance());
+        $logs = $writer->get_data([get_string('pluginname', 'block_motrain'),
+            get_string('privacy:path:logs', 'block_motrain')]);
+        $this->assertCount(1, $logs->data);
+        $this->assertEquals(100, $logs->data[0]->coins);
+
+        $mappings = $writer->get_data([get_string('pluginname', 'block_motrain'),
+            get_string('privacy:path:mappings', 'block_motrain')]);
+        $this->assertCount(2, $mappings->data);
+        $this->assertEquals('account1', $mappings->data[0]->accountid);
+        $this->assertEquals('account2', $mappings->data[1]->accountid);
+
+        $writer = writer::with_context($c1ctx);
+        $logs = $writer->get_data([get_string('pluginname', 'block_motrain'),
+            get_string('privacy:path:logs', 'block_motrain')]);
+        $this->assertCount(2, $logs->data);
+        $this->assertEquals(99, $logs->data[0]->coins);
+        $this->assertEquals(98, $logs->data[1]->coins);
+
+        $mappings = $writer->get_data([get_string('pluginname', 'block_motrain'),
+            get_string('privacy:path:mappings', 'block_motrain')]);
+        $this->assertEmpty($mappings);
+    }
+
+    /**
+     * Generate test data.
+     *
+     * @return array The associated items.
+     */
+    protected function generate_test_data() {
+        global $DB;
+
+        $dg = $this->getDataGenerator();
+        $now = time();
+
+        $c1 = $dg->create_course();
+        $c2 = $dg->create_course();
+        $u1 = $dg->create_user();
+        $u2 = $dg->create_user();
+        $u3 = $dg->create_user();
+        $u4 = $dg->create_user();
+
+        $DB->insert_record('block_motrain_playermap', [
+            'userid' => $u1->id,
+            'accountid' => 'account1',
+            'playerid' => 'playerid1',
+        ]);
+        $DB->insert_record('block_motrain_playermap', [
+            'userid' => $u1->id,
+            'accountid' => 'account2',
+            'playerid' => 'playerid1',
+            'blocked' => true,
+            'blockedreason' => 'EMAIL_ALREADY_USED',
+        ]);
+        $DB->insert_record('block_motrain_playermap', [
+            'userid' => $u3->id,
+            'accountid' => 'account1',
+            'playerid' => 'playerid3',
+        ]);
+
+        $DB->insert_record('block_motrain_log', [
+            'userid' => $u1->id,
+            'contextid' => context_system::instance()->id,
+            'coins' => 100,
+            'actionname' => 'some_action',
+            'actionhash' => '',
+            'timecreated' => 1,
+            'timebroadcasted' => 1,
+        ]);
+        $DB->insert_record('block_motrain_log', [
+            'userid' => $u1->id,
+            'contextid' => context_course::instance($c1->id)->id,
+            'coins' => 99,
+            'actionname' => 'some_action',
+            'actionhash' => '',
+            'timecreated' => 1,
+            'timebroadcasted' => 1,
+        ]);
+        $DB->insert_record('block_motrain_log', [
+            'userid' => $u1->id,
+            'contextid' => context_course::instance($c1->id)->id,
+            'coins' => 98,
+            'actionname' => 'some_action',
+            'actionhash' => '',
+            'timecreated' => 1,
+            'timebroadcasted' => 1,
+        ]);
+        $DB->insert_record('block_motrain_log', [
+            'userid' => $u1->id,
+            'contextid' => context_course::instance($c2->id)->id,
+            'coins' => 101,
+            'actionname' => 'some_action',
+            'actionhash' => '',
+            'timecreated' => 1,
+            'timebroadcasted' => 1,
+        ]);
+        $DB->insert_record('block_motrain_log', [
+            'userid' => $u2->id,
+            'contextid' => context_system::instance()->id,
+            'coins' => 10,
+            'actionname' => 'some_action',
+            'actionhash' => '',
+            'timecreated' => 1,
+            'timebroadcasted' => 1,
+        ]);
+        $DB->insert_record('block_motrain_log', [
+            'userid' => $u2->id,
+            'contextid' => context_course::instance($c1->id)->id,
+            'coins' => 9,
+            'actionname' => 'some_action',
+            'actionhash' => '',
+            'timecreated' => 1,
+            'timebroadcasted' => 1,
+        ]);
+
+        return [
+            'c1' => $c1,
+            'c2' => $c2,
+            'now' => $now,
+            'u1' => $u1,
+            'u2' => $u2,
+            'u3' => $u3,
+            'u4' => $u4,
+        ];
+    }
+
+    /**
+     * Compare context lists.
+     *
+     * @param object $contextlist The list.
+     * @param int[] $expectedids The IDs.
+     */
+    protected function assert_contextlist_equals($contextlist, $expectedids) {
+        $contextids = array_map('intval', $contextlist->get_contextids());
+        sort($contextids);
+        sort($expectedids);
+        $this->assertEquals($expectedids, $contextids);
+    }
+
+}
