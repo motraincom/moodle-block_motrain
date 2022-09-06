ffafdbd5efe23c93785a65d7e55b007bb2bb9590
Frédéric Massart <fred@branchup.tech>
2022-09-06 18:13:45 +0800

    Adding es_mx translations salvaged from legacy plugins

---
+++ lang/es_mx/block_motrain.php
@@ -0,0 +1,56 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language file.
+ *
+ * @package    block_motrain
+ * @copyright  2022 Mootivation Technologies Corp.
+ * @author     Frédéric Massart <fred@branchup.tech>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['adminscanearn'] = 'Admins pueden ganar monedas';
+$string['adminscanearn_desc'] = 'Active esta casilla para permitirle a los admins que ganen monedas.';
+$string['coins'] = 'Monedas';
+$string['completingacourse'] = 'Completando un curso';
+$string['configtitle'] = 'Título';
+$string['dashboard'] = 'Tablero';
+$string['disabled'] = 'Deshabilitado';
+$string['enabled'] = 'Habilitado';
+$string['eventcoinsearned'] = 'Monedas ganadas';
+$string['infopagetitle'] = 'Info';
+$string['invalidcoinamount'] = 'Invalid amount of coins';
+$string['leaderboard'] = 'Tablero de clasificación';
+$string['motrain:addinstance'] = 'Añadiendo un nuevo bloque Motrain';
+$string['motrain:myaddinstance'] = 'Añadiendo el bloque Motrain en el Tablero';
+$string['motrain:view'] = 'Viendo el contenido del bloque Motrain';
+$string['pleasewait'] = 'Por favor espere ...';
+$string['privacy:metadata:coinsgained'] = 'La información enviada a un servidor externo cuando un usuario gana monedas.';
+$string['privacy:metadata:coinsgained:coins'] = 'El número de monedas a añadir al monedero del usuario.';
+$string['privacy:metadata:coinsgained:reason'] = 'La razón para que el usuario reciba estas monedas.';
+$string['privacy:metadata:log'] = 'Bitácoras de eventos.';
+$string['privacy:metadata:log:coins'] = 'El número de monedas otorgadas para este evento';
+$string['privacy:metadata:log:contextid'] = 'El contexto en el que ocurre el evento';
+$string['privacy:metadata:log:timecreated'] = 'La fecha en la que ocurrió el evento';
+$string['privacy:metadata:log:userid'] = 'La ID del usuario';
+$string['privacy:path:logs'] = 'Bitácoras';
+$string['purchases'] = 'Mis compras';
+$string['setup'] = 'Configuración';
+$string['store'] = 'Tienda';
+$string['taskpushusers'] = 'Empujar usuarios seleccionados al tablero.';
