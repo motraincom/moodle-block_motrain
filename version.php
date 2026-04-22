0d0dd1745f76ba8b26237677b2783252303bbe0d
Frédéric Massart <fred@branchup.tech>
2026-04-22 14:35:58 +0800

    Release v1.14.5

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2026042200;
+$plugin->version  = 2026042201;
 $plugin->requires = 2017051500;     // Moodle 3.3.0.
-$plugin->release = '1.14.4';
+$plugin->release = '1.14.5';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'block_motrain';
 
